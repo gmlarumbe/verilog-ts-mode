04400ae34b66d8773381f79bbd4f7240cb8e62d5
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2023-09-07 20:30:52 +0200

    Fix header URL, update test submodule ref

--- verilog-ts-mode.el
+++ verilog-ts-mode.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2022-2023 Gonzalo Larumbe
 
 ;; Author: Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
-;; URL: https://github.com/gmlarumbe/verilog-ext
+;; URL: https://github.com/gmlarumbe/verilog-ts-mode
 ;; Version: 0.0.0
 ;; Keywords: Verilog, IDE, Tools
 ;; Package-Requires: ((emacs "29.1"))
