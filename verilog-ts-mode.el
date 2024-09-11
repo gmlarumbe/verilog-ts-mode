460d78f524cae677e86c8bbba2ce4e3c57e47cbf
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2024-09-11 21:30:48 +0200

    Update version to 0.2.1

--- verilog-ts-mode.el
+++ verilog-ts-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
 ;; URL: https://github.com/gmlarumbe/verilog-ts-mode
-;; Version: 0.2.0
+;; Version: 0.2.1
 ;; Keywords: Verilog, IDE, Tools
 ;; Package-Requires: ((emacs "29.1"))
 
